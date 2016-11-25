1adcd2cbda269e4f9d13d9356e1a41eaaccd8d8b
Abinash Mohanty <abinash.mohanty@gmail.com>
2016-11-25 11:55:20 +0530

    uploaded base bot files with readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# Slack Chat Bot - _Ramu Kaka_
+
+This is our 1st chat bot for GSIUXD UX community. Since this community is _India_ specific, we thought of giving this bot an interesting name, and why not! So let's call it _Ramu Kaka_ and this is how he looks 👳 at least for now.
+
+I'll come back to this documentation with more details as we make changes over time.
---
+++ index.js
@@ -0,0 +1,35 @@
+var Botkit = require('botkit')
+
+var token = process.env.SLACK_TOKEN
+
+var controller = Botkit.slackbot({
+  // reconnect to Slack RTM when connection goes bad
+  retry: Infinity,
+  debug: false
+})
+
+// Assume single team mode if we have a SLACK_TOKEN
+if (token) {
+  console.log('Starting in single-team mode')
+  controller.spawn({
+    token: token
+  }).startRTM(function (err, bot, payload) {
+    if (err) {
+      throw new Error(err)
+    }
+
+    console.log('Connected to Slack RTM')
+  })
+// Otherwise assume multi-team mode - setup beep boop resourcer connection
+} else {
+  console.log('Starting in Beep Boop multi-team mode')
+  require('beepboop-botkit').start(controller, { debug: true })
+}
+
+controller.on('bot_channel_join', function (bot, message) {
+  bot.reply(message, "I'm here!")
+})
+
+controller.hears(['hi'], ['ambient', 'direct_message','direct_mention','mention'], function (bot, message) {
+  bot.reply(message, 'Hello.')
+})
